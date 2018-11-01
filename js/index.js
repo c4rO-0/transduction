12e251b911951ed186da11ed4a18a807824843e0
Unknown <l365692902@gmail.com>
2018-11-01 09:31:34 +0800

    测试webview，我的alpha版也做出来了

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
 console.log(process.versions.electron)
+let status = "webviewSkype"
+$(document).ready(function(){
+    $("#switch").on("click", function(){
+        if(status=="webviewSkype"){
+            $("#webviewSkype").hide()
+            $("#webviewWechat").show()
+            status="webviewWechat"
+        }else if(status == "webviewWechat"){
+            $("#webviewSkype").show()
+            $("#webviewWechat").hide()
+            status="webviewSkype"
+        }
+    })
+})
--- main.js
+++ main.js
@@ -1,21 +1,17 @@
-const { app, BrowserWindow, BrowserView } = require('electron')
+const { app, BrowserWindow } = require('electron')
   
+const debug = /--debug/.test(process.argv[2])
+
   function createWindow () {
     // Create the browser window.
     win = new BrowserWindow({ width: 800, height: 600})
     
     win.loadFile('./html/index.html')
     // win.loadURL("https://web.skype.com/en/")
-    win.webContents.openDevTools()
+    if(debug){
+      win.webContents.openDevTools()
+    }
     win.setMenu(null)
-    let view = new BrowserView({
-      webPreferences:{
-        nodeIntegration:false
-      }
-    })
-    win.setBrowserView(view)
-    view.setBounds({x:10,y:10,width:800,height:600})
-    view.webContents.loadURL("https://web.skype.com/en/")
     // win.setProgressBar(0.5)
   }
   
