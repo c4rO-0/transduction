21fbf2c98ebce3e025b23c781bd8c502f710d902
Sheng Bi <shengbi@fhi-berlin.mpg.de>
2018-12-04 16:28:26 +0100

    修改表情符(未完)

--- js/webWechat.js
+++ js/webWechat.js
@@ -109,7 +109,13 @@ window.onload = function () {
                 parseInt(timeStr.substr(timeStr.indexOf(':') + 1)))
 
         }
-        let content = $(obj).find("div.info p.msg span.ng-binding[ng-bind-html='chatContact.MMDigest']").text()
+        let contentObj = $(obj).find("div.info p.msg span.ng-binding[ng-bind-html='chatContact.MMDigest']")
+        if ($(contentObj).length == 0) {
+            content = ''
+        }else{
+            content = $(contentObj).text()
+        }
+
         let nickName = $(obj).find("div.info h3.nickname span").text()
         let userID = $(obj).attr("data-username")
         let host =
@@ -119,7 +125,7 @@ window.onload = function () {
 
 
         let avatar = host + $(obj).find("div.avatar img").attr("src")
-        
+
 
 
         if ($("div[data-username='" + userID + "']").length == 0) {
@@ -179,7 +185,7 @@ window.onload = function () {
 
             return {
                 "userID": userID,
-                "time": time,
+                "time": time.getTime(),
                 "message": content,
                 "nickName": nickName,
                 "avatar": avatar,
@@ -217,31 +223,32 @@ window.onload = function () {
         let arrayObjUser = new Array();
         let arrayContent = new Array();
         records.map(function (record) {
-            // console.log("debug : ===========chat slide============")
-            // console.log("debug : ", "obs type : ", record.type)
-            // console.log("debug : ", "obs target : ") 
-            // console.log($(record.target))
-            // console.log("debug : ", "remove : ", $(record.removedNodes).length)
-            // console.log($(record.removedNodes))
+            console.log("debug : ===========chat slide============")
+            console.log("debug : ", "obs type : ", record.type)
+            console.log("debug : ", "obs target : ")
+            console.log($(record.target))
+            console.log("debug : ", "remove : ", $(record.removedNodes).length)
+            console.log($(record.removedNodes))
 
-            if ($(record.removedNodes).length == 0) {
-                // console.log("debug : ", "parent target : ") 
-                let obj = $(record.target).closest(".chat_item.slide-left.ng-scope")
-                // console.log( obj  )    
-                if (obj.length > 0) {
-                    let existed = false
-                    arrayObjUser.forEach((currentValue, index) => {
 
-                        if (!existed && $(currentValue).is(obj)) {
-                            existed = true
-                        }
+            let obj = $(record.target).closest(".chat_item.slide-left.ng-scope")
+            // console.log( obj  )    
+            if (obj.length > 0) {
+                let existed = false
+                arrayObjUser.forEach((currentValue, index) => {
 
-                    })
-                    if (!existed) {
-                        arrayObjUser.push(obj)
+                    if (!existed && $(currentValue).is(obj)) {
+                        existed = true
                     }
+
+                })
+                if (!existed) {
+                    arrayObjUser.push(obj)
                 }
-            } else {
+            }
+
+
+            if ($(record.removedNodes).length != 0) {
                 $(record.removedNodes).toArray().forEach((currentValue, index) => {
                     $(currentValue).children(".chat_item.slide-left.ng-scope").toArray().forEach((obj, idx) => {
                         let existed = false
@@ -261,8 +268,8 @@ window.onload = function () {
 
         })
 
-        // console.log("debug : ", "------array:user-----")
-        // console.log(arrayObjUser)
+        console.log("debug : ", "------array:user-----")
+        console.log(arrayObjUser)
 
         // console.log("debug : ", "------array:MSG-----")
         arrayObjUser.forEach((currentValue, index) => {
