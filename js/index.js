153976f0d14151ee39c0539f2f98d1b19ca817e1
Unknown <l365692902@gmail.com>
2019-02-24 17:45:50 +0800

    pin

--- js/index.js
+++ js/index.js
@@ -32,6 +32,8 @@ function listWebview() {
     })
 }
 
+let tdPinCoord = [0, 0]
+
 
 
 $(document).ready(function () {
@@ -932,6 +934,31 @@ $(document).ready(function () {
     }
     // =============================程序主体=============================
 
+    function followPin() {
+        let target = document.getElementById('td-pin')
+        let x = target.getBoundingClientRect().x
+        let y = target.getBoundingClientRect().bottom
+        tdPinCoord = [x, window.innerHeight - y]
+        window.scrollTo(0, 0)
+        document.getElementById('td-left').style.width = x + 'px'
+        document.getElementById('td-input').style.height = window.innerHeight - y + 'px'
+
+    }
+
+    //==============================UI==============================
+    $('#td-pin').draggable({
+        grid: [10, 10],
+        containment: "#td-pin-area",
+        drag: followPin,
+        stop: function (event, ui) {
+            followPin()
+            let target = document.getElementById('td-pin')
+            let y = target.getBoundingClientRect().bottom
+            target.style.bottom = window.innerHeight - y + 'px'
+            target.style.top = ''
+        },
+    })
+    followPin()
 
     // ===========================接收消息===========================
 
@@ -1000,8 +1027,15 @@ $(document).ready(function () {
     toggleWebview()
     // openDevtool('skype')
     window.onresize = () => {
-        console.log("===window resize====")
+        // console.log("===window resize====")
+        document.getElementById('td-pin').style.left = window.getComputedStyle(document.getElementById('td-left')).getPropertyValue('width')
+        document.getElementById('td-pin').style.bottom = window.getComputedStyle(document.getElementById('td-input')).getPropertyValue('height')
+        // console.log(window.getComputedStyle(document.getElementById('td-left')).getPropertyValue('width'))
     }
+    // $(window).resize(function () {
+    //     document.getElementById('td-pin').style.left = tdPinCoord[0] + 'px'
+    //     document.getElementById('td-pin').style.bottom = tdPinCoord[1] + 'px'
+    // })
 
 
     // =================extension click==================
