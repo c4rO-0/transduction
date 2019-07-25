64239f64f5a955d7499dcbe92b510473dd5ddd94
bsplu <ruc.bs.plu@hotmail.com>
2019-07-25 21:02:34 +0200

    ding : 对左侧有反应

--- js/webDingtalk.js
+++ js/webDingtalk.js
@@ -9,42 +9,92 @@ window.onload = function () {
     let logStatus = { "status": "offline" }
 
 
-    if ($('#menu-pannel').length == 0) {
-        console.log("********************offline***************************************")
-        logStatus.status = "offline"
-        core.WebToHost({ "logStatus": logStatus })
-        core.WebToHost({ "show": {} })
+    function callbackChat(mutationList, observer) {
+        // console.log('dingtalk left : ', mutationList)
+        mutationList.forEach((mutation, index) => {
+            if($(mutation.target).is('span.ng-binding.ng-hide')){
+                // 未读消息数增加
+                console.log('dingtalk convo changed : ', mutation, $(mutation.target).closest('conv-item'))
 
-        let callbackobsLogin = function (mutationList, observer) {
-            // console.log("log status changed : ", mutationList)
-            if ($('#menu-pannel').length > 0) {
-                logStatus.status = "online"
-                console.log("=======================online=====================================")
-                // console.log($("div.login"))
-                core.WebToHost({ "logStatus": logStatus })
-                core.WebToHost({ "hide": {} })
-    
-                observer.disconnect()
+                // ========爬取左边=========
+
             }
-        }
-        let obsLogin = new MutationObserver(callbackobsLogin);
-        obsLogin.observe($('body')[0], {
-            childList: true,
-            subtree: false,
-            characterData: false,
-            // attributeFilter: ["style"],
-            attributes: false, attributeOldValue: false
-        });        
-    }else{
-        logStatus.status = "online"
-        console.log("=======================online=====================================")
-        // console.log($("div.login"))
-        core.WebToHost({ "logStatus": logStatus })
-        core.WebToHost({ "hide": {} })
+
+            // mutation.addedNodes.forEach( (node,index) =>{
+            //     if($(node).is('conv-item')){
+            //         // 添加convo || 从以读到未读 
+            //         console.log('dingtalk convo changed : ', $(node))
+            //     }
+            // })
+            // if(mutation.removedNodes.length > 0){
+            //     if($(mutation.target).is('span.ng-binding.ng-hide')){
+            //         // 未读消息数增加
+            //         console.log('dingtalk convo changed : ', mutation, $(mutation.target).closest('conv-item'))
+            //     }
+            // }
+        })
     }
 
-
     $(document).ready(function () {
+
+
+        // 观察左侧消息变动
+        let obsChat = new MutationObserver(callbackChat);
+
+        if ($('#menu-pannel').length == 0) {
+            console.log("********************offline***************************************")
+            logStatus.status = "offline"
+            core.WebToHost({ "logStatus": logStatus })
+            core.WebToHost({ "show": {} })
+
+            let callbackobsLogin = function (mutationList, observer) {
+                // console.log("log status changed : ", mutationList)
+                if ($('#menu-pannel').length > 0) {
+                    logStatus.status = "online"
+                    console.log("=======================online=====================================")
+                    // console.log($("div.login"))
+                    core.WebToHost({ "logStatus": logStatus })
+                    core.WebToHost({ "hide": {} })
+                    observer.disconnect()
+
+                    obsChat.observe(document.getElementById('sub-menu-pannel'), {
+                        childList: true,
+                        subtree: true,
+                        characterData: true,
+                        characterDataOldValue: true,
+                        // attributeFilter: ["data-username"],
+                        attributes: false
+                        //  attributeOldValue: true
+                    });                    
+                }
+            }
+            let obsLogin = new MutationObserver(callbackobsLogin);
+            obsLogin.observe($('body')[0], {
+                childList: true,
+                subtree: false,
+                characterData: false,
+                // attributeFilter: ["style"],
+                attributes: false, attributeOldValue: false
+            });
+        } else {
+            logStatus.status = "online"
+            console.log("=======================online=====================================")
+            // console.log($("div.login"))
+            core.WebToHost({ "logStatus": logStatus })
+            core.WebToHost({ "hide": {} })
+
+            obsChat.observe(document.getElementById('sub-menu-pannel'), {
+                childList: true,
+                subtree: true,
+                characterData: false,
+                // characterDataOldValue: true,
+                // attributeFilter: ["data-username"],
+                attributes: false
+                // attributeOldValue: true
+            });
+
+        }
+
 
         core.WebReply((key, arg) => {
             return new Promise((resolve, reject) => {
@@ -63,7 +113,7 @@ window.onload = function () {
 
             })
 
-        })        
+        })
 
     })
 
