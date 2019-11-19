510970b190b163edbba7ef45b55b47cfee1f6441
Sheng Bi <shengbi@fhi-berlin.mpg.de>
2019-11-19 10:11:04 +0100

    添加whatsapp webview

---
+++ js/webWhatsapp.js
@@ -0,0 +1,12 @@
+window.onload = function () {
+
+    const core = require("../js/core.js")
+    const watchJS = require("../toolkit/watch-1.4.2.js")
+    // const http = require('http')
+    const fs = require('fs')
+    const { net } = require('electron').remote
+
+    let logStatus = { "status": "offline" }
+
+
+}
