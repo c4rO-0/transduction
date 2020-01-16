0718cb18eee781f80ee788954d6385e598db50f0
Sheng Bi <shengbi@fhi-berlin.mpg.de>
2020-01-16 16:48:18 +0100

    封装目前core所有函数

--- module/tdSys/index.js
+++ module/tdSys/index.js
@@ -3,8 +3,6 @@
  */
 
 const fs = require('fs')
-const {tdBasic} = require('tdBasic')
-
 
 class fileSend {
     constructor(name, path, webkitRelativePath, fileID = '', dataUrl = undefined) {
@@ -103,8 +101,45 @@ class fileSend {
 
     }
     clear() {
-        tdBasic.removeDir(path.join(this.pathRoot, this.fileID))
+        tdOS.removeDir(path.join(this.pathRoot, this.fileID))
     }
 }
 
-module.exports = {fileSend}
+/**
+ * all things about operating system
+ */
+class tdOS{
+    constructor(){
+
+    }
+    strUserAgentWin = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
+    AppleWebKit/537.36 (KHTML, like Gecko) \
+    Chrome/73.0.3683.121 Safari/537.36"
+    strUserAgentLinux = "Mozilla/5.0 (X11; Linux x86_64) \
+    AppleWebKit/537.36 (KHTML, like Gecko) \
+    Chrome/73.0.3683.121 Safari/537.36"
+    /**
+     * 删除系统某个文件夹及其子文件
+     * @param {string} dir 绝对路径
+     */
+    static removeDir(dir) {
+        let files = fs.readdirSync(dir)
+        for (var i = 0; i < files.length; i++) {
+            let childPath = path.join(dir, files[i]);
+            let stat = fs.statSync(childPath)
+            if (stat.isDirectory()) {
+                // 递归
+                removeDir(childPath);
+            } else {
+                //删除文件
+                fs.unlinkSync(childPath);
+            }
+        }
+        fs.rmdirSync(dir)
+    }
+
+
+
+}
+
+module.exports = {fileSend, tdOS}
