e1a6e6cbd71d93d6b818559a4ab3aacc60634062
Sheng Bi <shengbi@fhi-berlin.mpg.de>
2020-01-17 11:45:24 +0100

    static

--- module/tdSys/index.js
+++ module/tdSys/index.js
@@ -115,10 +115,10 @@ class tdOS{
     constructor(){
 
     }
-    strUserAgentWin = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
+    static strUserAgentWin = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
 AppleWebKit/537.36 (KHTML, like Gecko) \
 Chrome/73.0.3683.121 Safari/537.36"
-    strUserAgentLinux = "Mozilla/5.0 (X11; Linux x86_64) \
+    static strUserAgentLinux = "Mozilla/5.0 (X11; Linux x86_64) \
 AppleWebKit/537.36 (KHTML, like Gecko) \
 Chrome/73.0.3683.121 Safari/537.36"
     /**
