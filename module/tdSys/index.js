7b771464f553df40b46fdfcdd1b54e70c2733e92
Sheng Bi <shengbi@fhi-berlin.mpg.de>
2020-01-17 11:06:11 +0100

    fix bug : require

--- module/tdSys/index.js
+++ module/tdSys/index.js
@@ -5,6 +5,7 @@
 const fs = require('fs')
 const os = require('os')
 const path = require('path')
+const mkdirp = require('mkdirp')
 
 class fileSend {
     constructor(name, path, webkitRelativePath, fileID = '', dataUrl = undefined) {
